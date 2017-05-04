a1dbf5f9a2bc5fafbd1f6f30aaea50e46c35d2b3
Jenna Cooper and Tyler Allen <tyler@>
2017-05-04 14:26:23 -0700

    complete roman numeral webpage

---
+++ README.md
@@ -0,0 +1,42 @@
+# Roman Numerals
+
+## Created by Tyler Allen & Jenna Cooper
+
+
+## User Story
+
+*
+
+
+## Specs
+
+| Objective | Example: Input | Example: Output |
+|:-------------:|:-------------:|:-------------:|
+| Output nothing when 0 is inputted | 0 |  |
+| Output nothing when a non-number is inputted | hello | Nice Try!! |
+| Output a single symbol | 10 | X |
+| Output multiple symbols  | 12 | XII |
+
+
+## Setup/Installation Requirements
+
+  * clone repository in GitHub
+  * open index.html in web browser
+
+
+
+## Bugs
+There are no known bugs at this time, but please contact the creator with questions or concerns regarding this application.
+
+
+## Technologies Used
+
+  * jQuery
+  * CSS - Bootstrap
+  * HTML
+
+
+## Licensing
+This application features MIT licensing.
+
+Copyright &copy; 2017 **Tyler Allen & Jenna Cooper** All Rights Reserved.
--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,52 @@
 //Business Logic (Back-End)
+var romans = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+var numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 
+
+var romanConverter = function(input) {
+var romanNumbers = "";
+  for (var index = 0; index < romans.length; index += 1) {
+    if (input >= numbers[index]) {
+      romanNumbers = romanNumbers + romans[index];
+      input = input - numbers[index];
+      index = index - 1;
+    }
+  }
+  return romanNumbers;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var roman = function(input){
 //
-
-
-var roman = function(input){
-
-  if (input === 0) {
-    return " ";
-  }
-  else if (input > 0) {
-    // run the function to convert number to roman numeral
-  }
-  else {
-    return "Nice Try!!";
-  }
-};
+//   if (input === 0) {
+//     return " ";
+//   }
+//   else if (input > 0) {
+//     // run the function to convert number to roman numeral
+//   }
+//   else {
+//     return "Nice Try!!";
+//   }
+// };
 
 //User Interface Logic (Front-End)
 $(document).ready(function(){
   $("form#roman-numeral").submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($("input#number").val());
-    var result = roman(userInput);
+    var result = romanConverter(userInput);
     $("#result").text(result);
     $("#result").show();
   });
